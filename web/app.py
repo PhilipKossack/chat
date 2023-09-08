540e338a87a505c22229c653375d416ae44962e3
Philip Kossack <p.kossack@posteo.de>
2023-09-08 07:45:30 +0200

    initial

---
+++ web/app.py
@@ -0,0 +1,234 @@
+from langchain import PromptTemplate, LLMChain
+from src.llm import build_llm
+import requests
+import json
+print("start")
+llm = build_llm()
+
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Any
+import pickle
+from uuid import uuid4
+
+import threading
+import queue
+
+class jobStatus():
+
+    def __init__(self):
+        self.jobsByToken = {}
+
+    def addJob(self,token,uuid,prompt,question):
+        if token in self.jobsByToken:
+            self.jobsByToken[token][uuid] = {'status':'queued','prompt':prompt,'question':question}
+        else:
+            self.jobsByToken[token] = {uuid:{'status':'queued','prompt':prompt,'question':question}}
+        
+    def removeJob(self,token,uuid):
+        if token in self.jobsByToken:
+            if uuid in self.jobsByToken[token]:
+                del self.jobsByToken[token][uuid]
+                return True
+        return False
+    
+    def addAnswer(self,token,uuid,answer):
+        if token in self.jobsByToken:
+            if uuid in self.jobsByToken[token]:
+                self.jobsByToken[token][uuid]['answer'] = answer
+                return True
+        return False
+    
+    def updateStatus(self,token,uuid,status):
+        if token in self.jobsByToken:
+            if uuid in self.jobsByToken[token]:
+                self.jobsByToken[token][uuid]['status'] = status
+                return True
+        return False
+    
+    def getJobStatus(self,token,uuid):
+        if token in self.jobsByToken:
+            if uuid in self.jobsByToken[token]:
+                status = self.jobsByToken[token][uuid]
+                status['uuid'] = uuid
+                return self.jobsByToken[token][uuid]
+        return {'uuid':'unknown','status':'unknown','prompt':'unknown','question':'unknown','answer':'unknown'}
+    
+    def getAllJobsForToken(self,token):
+        if token in self.jobsByToken:
+            return self.jobsByToken[token]
+        return {'unknown':{'status':'unknown','prompt':'unknown','question':'unknown','answer':'unknown'}}
+
+    def getAllStatus(self):
+        return self.jobsByToken
+            
+  
+class MainProcessor (threading.Thread):
+    def __init__(self,taskLock,taskQueue):
+        super().__init__(target="MainProcessor")
+       
+        self.taskLock = taskLock
+        self.taskQueue = taskQueue
+        
+    def translate(self,inLang,outLang,text):
+        req = {'from':inLang,'to':outLang,'source':text.strip()}
+        print(text)
+        print(text.strip())
+        resp = requests.post('http://translate/api/translate',json=req)
+        try:
+            resp = json.loads(resp.text)
+            if 'translation' in resp:
+                return resp['translation']
+            else:
+                return False
+        except:
+            return False
+
+
+    def run(self):
+        while True:
+            job = self.taskQueue.get(block=True)
+            jobStat.updateStatus(job['token'],job['uuid'],"processing")
+            item = jobStat.getJobStatus(job['token'],job['uuid'])
+
+            tPrompt = self.translate('de','en',item['prompt'])
+            tQuestion = self.translate('de','en',item['question'])
+            if tPrompt:
+                prompt_template = tPrompt + " {Question}?"
+            else:
+                prompt_template = item['prompt'] + " {Question}?"
+            llm_chain = LLMChain(
+                llm=llm,
+                prompt=PromptTemplate.from_template(prompt_template)
+            )
+            if tQuestion:
+                answer = llm_chain(tQuestion)
+            else:
+                answer = llm_chain(item['question'])
+            
+            tAnswer = self.translate('en','de',answer['text'])
+            if tAnswer:
+                jobStat.addAnswer(job['token'],job['uuid'],tAnswer)
+            else:
+                jobStat.addAnswer(job['token'],job['uuid'],answer['text'])
+            jobStat.updateStatus(job['token'],job['uuid'],"finished")
+
+
+
+
+
+def dumpTokens(tokens):
+    pickle.dump(tokens, open("/config/tokens.pickle", "wb"))
+
+try:
+    tokens = pickle.load(open("/config/tokens.pickle", "rb"))
+except (OSError, IOError) as e:
+    tokens = {}
+    dumpTokens(tokens)
+
+def generate_token(quota,description = ""):
+    token = uuid4().hex
+    tokens[token]={'quota':quota,'description':description}
+    dumpTokens(tokens)
+    return token
+
+def revoke_token(token):
+    if token in tokens:
+        del(tokens[token])
+        dumpTokens(tokens)
+        return "OK"
+    else:
+        return "Token tot found"
+
+def check_token(token):
+    if token in tokens:
+        if tokens[token]['quota'] == -1:
+            return True
+        if tokens[token]['quota'] > 0:
+            tokens[token]['quota'] -= 1
+            return True    
+    return False
+
+def token_details(token):
+    if token in tokens:
+        return tokens[token]
+    return {'quota':0,'description':'Not existent'}
+
+supertoken = ""
+
+
+
+jobStat = jobStatus()
+
+taskLock = threading.Lock()
+taskQueue = queue.Queue(1000)
+
+thread = MainProcessor(taskLock,taskQueue)
+thread.start()
+
+
+
+
+
+
+app = FastAPI()
+
+class Item(BaseModel):
+    prompt: str
+    question: str
+    token: str
+
+class TokenCreation(BaseModel):
+    supertoken: str
+    quota: int | None = 100
+    description: str | None = "unknown" 
+
+class TokenRevoke(BaseModel):
+    supertoken: str
+    token: str
+
+class Status(BaseModel):
+    token: str
+    uuid: str | None = "All"
+
+
+@app.post("/getStatus/")
+async def get_status(status: Status) -> Any:
+    if status.uuid == "All":
+        stat = jobStat.getAllJobsForToken(status.token)
+    else:
+        stat = jobStat.getJobStatus(status.token,status.uuid)
+    return stat
+
+@app.post("/createToken/")
+async def create_token(token: TokenCreation) -> Any:
+    if token.supertoken == supertoken:
+        token = generate_token(token.quota,token.description)
+        return {"token":token}
+    else:
+        return {"result": "Acces denied."}
+    
+@app.post("/revokeToken/")
+async def create_token(token: TokenRevoke) -> Any:
+    if token.supertoken == supertoken:
+        result = revoke_token(token.token)
+        return {"result": result}
+    else:
+        return {"result": "Acces denied."}
+
+@app.post("/generate/")
+async def generate_text(item: Item) -> Any:
+    if(check_token(item.token)):
+        uuid = uuid4().hex
+        jobStat.addJob(item.token,uuid,item.prompt,item.question)
+        job = {'token':item.token,'uuid':uuid}
+        try:
+            taskQueue.put(job)
+        except:
+            jobStat.updateStatus(item.token,uuid,"failed")
+        result = jobStat.getJobStatus(item.token,uuid)
+    else:
+        result = "Access denied."
+    return result
+
---
+++ web/src/llm.py
@@ -0,0 +1,43 @@
+from langchain.llms import CTransformers
+from dotenv import find_dotenv, load_dotenv
+import box
+import yaml
+import requests
+import os
+
+def download_file(url, filename):
+    local_filename = filename
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):  
+                f.write(chunk)
+    return local_filename
+
+# Load environment variables from .env file
+load_dotenv(find_dotenv())
+
+# Import config vars
+with open('/config/config.yml', 'r', encoding='utf8') as ymlfile:
+    cfg = box.Box(yaml.safe_load(ymlfile))
+
+
+
+url= cfg.MODEL_DOWNLOAD_URL
+filename = cfg.MODEL_BIN_PATH
+
+
+def build_llm():
+    if not os.path.exists(filename):
+        print("Specified Model not found. Downloading Model...")
+        download_file(url,filename)
+        print("Download complete.")
+    # Local CTransformers model
+    llm = CTransformers(model=cfg.MODEL_BIN_PATH,
+                        model_type=cfg.MODEL_TYPE,
+                        #config={'max_new_tokens': cfg.MAX_NEW_TOKENS,
+                                #'temperature': cfg.TEMPERATURE}
+                        )
+
+    return llm
+
